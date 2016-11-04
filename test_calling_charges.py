f21426f5d13aaec0da048dd43be1db1600ab56a5
Jonathan Lintott <Jonathan_Lintott@outlook.com>
2016-11-04 08:09:08 +0000

    Test the unit testing

--- test_calling_charges.py
+++ test_calling_charges.py
@@ -1,1 +1,29 @@
 __author__ = 'Jonathan Lintott'
+
+# Standard library imports
+import unittest
+
+# Web scraping imports
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+class CallingChargeSearch(unittest.TestCase):
+    """
+    For testing of the calling charge scraping script in 'calling_charges.py'
+    """
+    def setUp(self):
+        binary = FirefoxBinary('C:/Users/jli199/AppData/Local/Mozilla Firefox/firefox.exe')
+        self.driver = webdriver.Firefox(firefox_binary=binary)
+
+    def test_wait(self):
+        self.driver.get('http://www.python.org')
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    # Run tests defined within this script
+    unittest.main()
+
